2a7cedb72be189c25d5b1a76091f6b409e472f93
NeblioTeam <neblioteam@gmail.com>
2019-01-15 12:33:20 -0500

    Update stats.js

--- models/stats.js
+++ models/stats.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
- 
+
 var StatsSchema = new Schema({
   coin: { type: String },
   count: { type: Number, default: 1 },
@@ -10,7 +10,8 @@ var StatsSchema = new Schema({
   supply: { type: Number, default: 0 },
   //last_txs: { type: Array, default: [] },
   connections: { type: Number, default: 0 },
-  last_price: { type: Number, default: 0 },
+  last_price_btc: { type: Number, default: 0 },
+  last_price_usd: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('coinstats', StatsSchema);
