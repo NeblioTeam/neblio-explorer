03615b1a95cd4bac775d5dd3b5a0163b28cb6faa
NeblioTeam <neblioteam@gmail.com>
2020-10-27 13:26:10 -0400

    debug statement

--- iquidus-sync/explorer_sync.py
+++ iquidus-sync/explorer_sync.py
@@ -884,6 +884,7 @@ class Tx(object):
             if i.get("is_stake") is not None:
                 del i["is_stake"]
             if i.get("is_cold_stake") is not None:
+                logger.info("Found cold stake output: "+i)
                 is_cold_stake = True
                 del i["is_cold_stake"]
         ret = {
