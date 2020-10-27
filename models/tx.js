6f2523d58ba92ea28694742dbb2073baa79cba4b
NeblioTeam <neblioteam@gmail.com>
2020-10-27 15:37:49 -0400

    Update tx.js

--- models/tx.js
+++ models/tx.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
- 
+
 var TxSchema = new Schema({
   txid: { type: String, lowercase: true, unique: true, index: true},
   vin: { type: Array, default: [] },
   vout: { type: Array, default: [] },
   total: { type: Number, default: 0 },
   has_token: { type: Boolean, default: false },
+  is_cold_stake: { type: Boolean, default: false },
   timestamp: { type: Number, default: 0 },
   blockhash: { type: String },
   blockindex: {type: Number, default: 0},
